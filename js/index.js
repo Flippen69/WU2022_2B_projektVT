69932dce672e416f44c092bf9f4fc51d78febef9
Filiph <83348675+Flippen69@users.noreply.github.com>
2022-04-03 23:05:55 +0200

    Sista puchen innan inlämnning
    Det har varit riktigt skoj med detta projektet.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
 const burger = document.querySelector('.burger')
 const nav = document.querySelector('.navbar_wrapper')
-const arrow = document.querySelector('.arrow')
 
 function toggleMenu() {
     nav.classList.toggle('navbar_active')
     burger.classList.toggle('toggle')
-    arrow.classList.toggle('non_arrow')
 }
 
 burger.addEventListener('click', toggleMenu)
